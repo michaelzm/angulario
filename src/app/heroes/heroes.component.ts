f8f152014f1dc2c1614aff25aa6497867c3c4110
Michael Zemler <michael.zemler@hotmail.com>
2018-11-21 20:43:52 +0100

    Start implementing rxjs
    services part

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,15 +10,24 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+
   selectedHero: Hero;
+  heroes: Hero[];
   onSelect(hero: Hero): void {
-    this.selectedHero = hero; 
+    this.selectedHero = hero;
   }
 
-  constructor() { }
+
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
 
 }
